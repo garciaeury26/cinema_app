17ecf1afd8baf34f2d7dfa431466b2835a833d36
eury <garciaeury26@gmail.com>
2023-07-05 19:32:09 -0400

    agregar,borrar favorito

--- lib/infreastucture/datasouerces/isar_datasource.dart
+++ lib/infreastucture/datasouerces/isar_datasource.dart
@@ -1,22 +1,61 @@
 import 'package:cinema_app/domain/entities/movie.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 import '../../domain/datasources/local_storage_datasource.dart';
 
 class IsarDataSource extends LocalStorageDataSource {
-  @override
-  Future<void> toggleFavorite(Movie movie) {
-    throw UnimplementedError();
+  late Future<Isar> db;
+
+  IsarDataSource() {
+    db = openDb();
+  }
+
+  Future<Isar> openDb() async {
+    //obtiene el directorio de documentos de la aplicación
+    final dir = await getApplicationDocumentsDirectory();
+
+    if (Isar.instanceNames.isEmpty) {
+      // MovieSchema => es lo que genera el paque segundo la entidad
+      return await Isar.open([MovieSchema], directory: dir.path);
+    }
+
+    //  para mantener la consistencia del tipo de retorno.
+    return Future.value(Isar.getInstance());
   }
 
   @override
-  Future<bool> isMovieInFavorites(int movieId) {
-    // TODO: implement isMovieInFavorites
-    throw UnimplementedError();
+  Future<void> toggleFavorite(Movie movie) async {
+    final isar = await db;
+    final isMovieInFavorites =
+        await isar.movies.filter().idEqualTo(movie.id).findFirst();
+
+    // agregar
+    if (isMovieInFavorites == null) {
+      isar.writeTxnSync(() => isar.movies.putSync(movie));
+    }
+
+    // borrar
+    isar.writeTxnSync(() => isar.movies.deleteSync(movie.id));
   }
 
   @override
-  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
-    // TODO: implement loadMovies
-    throw UnimplementedError();
+  Future<bool> isMovieInFavorites(int movieId) async {
+    final isar = await db;
+
+    // el metodo =>  idEqualTo es generado segun la propiedad a la que se quiere manaipular
+    final Movie? isInFavoriteList =
+        await isar.movies.filter().idEqualTo(movieId).findFirst();
+
+    return isInFavoriteList != null;
+  }
+
+  @override
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) async {
+    final isar = await db;
+    final movies =
+        await isar.movies.where().offset(offset).limit(limit).findAll();
+
+    return movies;
   }
 }
